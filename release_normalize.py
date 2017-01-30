4b00696d06cead5521ad7b6d0e668a9820992e6f
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2017-01-30 14:17:09 +0000

    more modular

--- release_normalize.py
+++ release_normalize.py
@@ -21,19 +21,22 @@ parser.add_option("--output-lib", dest="output_lib",
                   help="output lib file", metavar="FILE")
 
 
+def main(args=None):
+    (options, args) = parser.parse_args(args)
 
-(options, args) = parser.parse_args()
+    if options.input_object and options.output_object:
+        print("stripping an object file")
+        obj.strip(options.input_object, options.output_object)
+    elif options.input_image and options.output_image:
+        print("fixing an image")
+        pe = pefile.PE(options.input_image)
+        pe.default_timestamp()
+        pe.write(options.output_image)
+        pe.close()
+    elif options.input_lib and options.output_lib:
+        lib.fix_lib_timestamps(options.input_lib, options.output_lib)
+    else:
+        parser.print_help()
 
-if options.input_object and options.output_object:
-    print("stripping an object file")
-    obj.strip(options.input_object, options.output_object)
-elif options.input_image and options.output_image:
-    print("fixing an image")
-    pe = pefile.PE(options.input_image)
-    pe.default_timestamp()
-    pe.write(options.output_image)
-    pe.close()
-elif options.input_lib and options.output_lib:
-    lib.fix_lib_timestamps(options.input_lib, options.output_lib)
-else:
-    parser.print_help()
+if __name__ == '__main__':
+    main()
